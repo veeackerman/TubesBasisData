27e936b7694a24c9b997eacc20aa1b4af7034dd7
veeackerman <82373301+veeackerman@users.noreply.github.com>
2021-06-02 20:53:04 +0700

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,17 +1,17 @@
 <?php 
     //Development Connection
-    $host = '127.0.0.1';
-    $db = 'dbtubes';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8mb4';
+    //$host = '127.0.0.1';
+    //$db = 'dbtubes';
+    //$user = 'root';
+    //$pass = '';
+    //$charset = 'utf8mb4';
 
     //Remote Database Connection
-    //$host = 'remotemysql.com';
-    //$db = 'XYycwXqwd5';
-    //$user = 'XYycwXqwd5';
-    //$pass = 'x7YxpAGtP4';
-    //$charset = 'utf8mb4';
+    $host = 'remotemysql.com';
+    $db = '0FW7TSXnLr';
+    $user = '0FW7TSXnLr';
+    $pass = 'ezQMSjtuRK';
+    $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
